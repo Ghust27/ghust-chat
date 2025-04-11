12074eaa4e1612a4946d222a08622c959908fe97
Ghust <andregustavoteixeira58@gmail.com>
2025-04-11 00:06:42 -0300

    auth.controller, auth.routes, cloudinary e middleware.

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -1,6 +1,7 @@
 import bcrypt from 'bcryptjs'
 import User from '../models/user.model.js'
 import {generateToken} from "../lib/utils.js"
+import cloudinary from '../lib/cloudinary.js'
 
 export const signup = async (req,res)=>{
     try{
@@ -42,10 +43,67 @@ export const signup = async (req,res)=>{
     }
 }
 
-export const login = (req,res)=>{
-    
+export const login = async (req,res)=>{
+    try{
+        const {email, password} = req.body
+        const user = await User.findOne({email})
+        if(!user){
+            return res.status(400).json({message:"Invalid credencials."})
+        }
+
+        const isMatch = await bcrypt.compare(password, user.password)
+        if(!isMatch){
+            return res.status(400).json({message:"Invalid credencials."})
+        }
+
+        generateToken(user._id,res)
+        res.status(200).json({
+            _id:user._id,
+            fullName:user.fullName,
+            email:user.email,
+            profilePic:user.profilePic
+        })
+
+    }catch (error){
+        console.log("Error in login controller",error.message)
+        res.status(500).json({message:"Internal server error."})
+    }
 }
 
 export const logout = (req,res)=>{
-    
+    try{
+        res.cookie("jwt","",{maxAge:0})
+        res.status(200).json({message:"Logged out successfully."})
+    }catch (error){
+        console.log("Error in logout controller",error.message)
+        res.status(500).json({message:"Internal server error."})
+    }
+}
+
+export const updateProfile = async (req,res)=>{
+    try{
+        const {profilePic} = req.body
+        const userId = req.user._id
+
+        if(!profilePic){
+            return res.status(400).json({message:"Profile pic is required."})
+        }
+        const uploadResponde = await cloudinary.uploader.upload(profilePic)
+        const updatedUser = await User.findByIdAndUpdate(userId, {profilePic:uploadResponse.secure_url},{new:true})
+
+        res.status(200).json(updatedUser)
+
+    }catch (error){
+        console.log("Error in update profile controller",error.message)
+        res.status(500).json({message:"Internal server error."})
+    }
+}
+
+export const checkAuth = (req,res)=>{
+    try{
+        res.status(200).json(req.user)
+    }catch (error){
+        console.log("Error in checkAuth controller",error.message)
+        res.status(500).json({message:"Internal server error."})
+    }
 }
--- backend/src/index.js
+++ backend/src/index.js
@@ -3,11 +3,13 @@ import authRoutes from "./routes/auth.route.js"
 import dotenv from 'dotenv'
 dotenv.config()
 import {connectDB} from "./lib/db.js"
+import cookieParser from 'cookie-parser'
 
 const app = express() 
 const port = process.env.PORT || 5001
 
 app.use(express.json())
+app.use(cookieParser())
 
 app.use('/api/auth',authRoutes)
 
